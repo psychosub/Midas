271a964c65a3c70995ddb09c8f54138df2630210
Grenade Visuals <65842130+GrenadeVisuals@users.noreply.github.com>
2020-09-15 18:53:06 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ import requests
 import discord
 import os
 import time
+import pypresence
+from pypresence import Presence
 
 from discord.ext import commands
 
@@ -13,6 +15,10 @@ async def on_ready():
     print("Online and Ready")
     status = discord.Activity(type=discord.ActivityType.playing, name="-help")
     await client.change_presence(activity=status)
+    rpc = Presence("754666157826506794")
+    rpc.connect()
+    rpc.update(state="Example", details="Example", large_image="test", start=time.time())
+    
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
